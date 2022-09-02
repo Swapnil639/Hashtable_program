ae77736c3c50901127ca10271d0b927ec7c974c6
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-02 17:37:18 +0530

    Added :Using Hashtable to find frequency of the another sentence

--- src/com/bridgelabz/HashtableMain.java
+++ src/com/bridgelabz/HashtableMain.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public class HashtableMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to hashtable program ");
-            Hashtable<String, Integer> hashtable = new Hashtable<>();
-            String input = "To be or not to be";
-            String[] inputArray = input.toLowerCase().split(" ");
-            for (String str : inputArray) {
-                if (hashtable.containsKey(str)) {
-                    hashtable.put(str, hashtable.get(str) + 1);
-                } else {
-                    hashtable.put(str, 1);
-                }
-            }
-            for (Map.Entry entry : hashtable.entrySet()) {
-                System.out.println(entry.getKey() + "  " + entry.getValue());
+        System.out.println("Welcome to HashTable Program ");
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        String input = " Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] inputArray = input.toLowerCase().split(" ");
+        for (String str : inputArray) {
+            if (hashtable.containsKey(str)) {
+                hashtable.put(str, hashtable.get(str) + 1);
+            } else {
+                hashtable.put(str, 1);
             }
         }
+        for (Map.Entry entry : hashtable.entrySet()) {
+            System.out.println(entry.getKey() + " --> " + entry.getValue());
+        }
     }
+}
