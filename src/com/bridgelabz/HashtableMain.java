58b32f151e64a17c6a6e7e8d165a9b0df55f67fe
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-02 18:07:46 +0530

    Added :removing the avoidible word in the sentence

--- src/com/bridgelabz/HashtableMain.java
+++ src/com/bridgelabz/HashtableMain.java
@@ -3,21 +3,23 @@ package com.bridgelabz;
 import java.util.Hashtable;
 import java.util.Map;
 
+
 public class HashtableMain {
     public static void main(String[] args) {
         System.out.println("Welcome to HashTable Program ");
         Hashtable<String, Integer> hashtable = new Hashtable<>();
         String input = " Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-        String[] inputArray = input.toLowerCase().split(" ");
+        input = input.replace("avoidable", "");     // removing avoidable word replace with space
+        String[] inputArray = input.toLowerCase().split(" ");  // It is converting to lowercase and also split it like every word
         for (String str : inputArray) {
-            if (hashtable.containsKey(str)) {
-                hashtable.put(str, hashtable.get(str) + 1);
+            if (hashtable.containsKey(str)) {    // checking every word
+                hashtable.put(str, hashtable.get(str) + 1);  // more than one time incremented by the value
             } else {
-                hashtable.put(str, 1);
+                hashtable.put(str, 1); // else part  only one time word in --> input
             }
         }
         for (Map.Entry entry : hashtable.entrySet()) {
-            System.out.println(entry.getKey() + " --> " + entry.getValue());
+            System.out.println(entry.getKey() + " --> " + entry.getValue());// print the all frequency with value and string
         }
     }
 }
